abf5a474cdeae1a4a3b19c0ca42eb1781ba1500c
favoritofood <favoritofood@favoritofoods-MacBook-Air.local>
2023-03-20 19:09:28 +0700

    inital commit

---
+++ resources/views/cv.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV PSW</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div style="width: 100vw; height: 60vh; display: flex; align-items: center; justify-content: center; font-family: 'Poppins', sans-serif;">
+        <div class="container">
+            <img class="profile" src={{ asset('images/wn.png') }} alt="" style="display:flex; justify-content;center; width: 200px; height: 200px; object-fit: cover; border-radius: 50%; margin-left:40px; margin-right:40px;">
+            <h1 style="text-align:center; margin-bottom:0px">Winson Napoleon</h1>
+            <p style="text-align:center">Hi! I'm a student</p>
+            <a class="btn" href="https://www.instagram.com/wiinson_/" target="_blank">Check me out!</a>
+        </div>
+    </div>
+  
+    <div class="about">
+    <div style="display:flex; flex-direction:row; gap:75px ;justify-content:space-around; font-family: 'Poppins', sans-serif; margin:50px">
+        <div>
+            <h1 style="text-align:center">About Me</h1>
+            <p style="text-align:center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti quam vero consequuntur. Mollitia, repellat iure doloribus libero unde magnam?</p>
+        </div>
+        <div>
+            <h1 style="text-align:center">What to expect</h1>
+            <p style="text-align:center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti quam vero consequuntur. Mollitia, repellat iure doloribus libero unde magnam?</p>
+        </div>
+        <div>
+            <h1 style="text-align:center">See my work</h1>
+            <p style="text-align:center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti quam vero consequuntur. Mollitia, repellat iure doloribus libero unde magnam?</p>
+        </div>
+    </div>
+    </div>
+
+    <div>
+       
+    </div>
+
+</div>
+  </body>
+
+</html> 
